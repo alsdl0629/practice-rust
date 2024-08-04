fb25672b494bbb3116b78440d71d3ac3dd586dc5
alsdl0629 <ehfehf0629@gmail.com>
2024-08-04 22:41:40 +0900

    변수

---
+++ rust_grammar/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let mut x = 5;
+    println!("x is: {x}");
+    x = 6;
+    println!("x is: {x}");
+
+    let y = 5;
+
+    let y = 6;
+
+    {
+        let y = y * 2;
+        println!("y is {y}");
+    }
+
+    println!("y is = {y}");
+
+    let mut spaces = "   ";
+    println!("{spaces}");
+    spaces = spaces.len();
+    println!("{spaces}");
+}
