c41ef0b746ff541811c32e9ce73500448233b9b8
alsdl0629 <ehfehf0629@gmail.com>
2024-08-05 14:20:29 +0900

    fibonacci

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -5,14 +5,18 @@ use std::io;
 
 fn main() {
     // 화씨 온도와 섭씨 온도 간 변환하기
-    println!("섭씨 or 화씨 온도를 입력해 주세요!");
+    // println!("섭씨 or 화씨 온도를 입력해 주세요!");
+    //
+    // let mut temperature = String::new();
+    //
+    // io::stdin()
+    //     .read_line(&mut temperature)
+    //     .expect("입력 실패");
+    //
+    // let temperature = calculate_temperature::convert_temperature_unit(temperature);
+    // println!("temperature = {temperature}");
 
-    let mut temperature = String::new();
-
-    io::stdin()
-        .read_line(&mut temperature)
-        .expect("입력 실패");
-
-    let temperature = calculate_temperature::convert_temperature_unit(temperature);
-    println!("temperature = {temperature}");
+    // n번째 피보나치 수 생성하기
+    let fibonacci = fibonacci::fibonacci(6);
+    println!("fibonacci = {fibonacci}");
 }
