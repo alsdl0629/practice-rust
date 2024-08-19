59fd7e1ef3e895375334e0c75d65258ab0331f1e
alsdl0629 <ehfehf0629@gmail.com>
2024-08-19 17:46:16 +0900

    extract lib.rs

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,30 @@
+// 라이브러리 크레이트
+
+use std::fs;
+use std::error::Error;
+
+pub fn run(config: Args) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?; // 파일 열기
+
+    println!("With text:\n{contents}");
+
+    Ok(()) // 에러 처리만 하겠다는 뜻
+}
+
+pub struct Args {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Args {
+    pub fn build(args: &Vec<String>) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("query, file_path를 입력해주세요!")
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        return Ok(Args { query, file_path })
+    }
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,11 +1,21 @@
-use std::env;
+use std::{env, process};
+use minigrep::{Args, run};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Args::build(&args)
+        .unwrap_or_else(|error| {
+            println!("Problem parsing arguments: {error}");
+            process::exit(1);
+        });
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    // return 값이 unit이기 때문에 unwrap을 사용할 필요 없음
+    if let Err(error) = run(config) {
+        println!("Application error: {error}");
+        process::exit(1);
+    }
 }
