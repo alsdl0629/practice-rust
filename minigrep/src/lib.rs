9ad8678b7b4dcddb6b88dafde5afcc2e99878dbc
alsdl0629 <ehfehf0629@gmail.com>
2024-08-19 19:18:54 +0900

    priority env

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,12 +1,17 @@
 // 라이브러리 크레이트
-
-use std::fs;
+use std::{env, fs};
 use std::error::Error;
 
 pub fn run(config: Args) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?; // 파일 열기
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -16,18 +21,34 @@ pub fn run(config: Args) -> Result<(), Box<dyn Error>> {
 pub struct Args {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Args {
     pub fn build(args: &Vec<String>) -> Result<Self, &'static str> {
         if args.len() < 3 {
-            return Err("query, file_path를 입력해주세요!")
+            return Err("query, file_path를 입력해주세요!");
         }
 
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        return Ok(Args { query, file_path })
+        // 환경 변수 우선순위
+        let ignore_case_by_command_line = if (&args[3]).parse::<u32>().unwrap() == 1 {
+            true
+        } else {
+            false
+        };
+
+        let ignore_case_by_env = env::var("IGNORE_CASE").is_ok();
+
+        let result = if ignore_case_by_env {
+            Ok(Args { query, file_path, ignore_case: ignore_case_by_env })
+        } else {
+            Ok(Args { query, file_path, ignore_case:ignore_case_by_command_line })
+        };
+
+        result
     }
 }
 
@@ -44,18 +65,47 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    let query = query.to_lowercase();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
