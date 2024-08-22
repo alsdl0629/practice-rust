77d80c646bec95979bb32abb5e12cdafff667472
alsdl0629 <ehfehf0629@gmail.com>
2024-08-22 13:17:07 +0900

    print only error

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,11 +6,13 @@ fn main() {
 
     let config = Args::build(&args)
         .unwrap_or_else(|error| {
+            eprintln!("Problem parsing arguments: {error}");
             process::exit(1);
         });
 
     // return 값이 unit이기 때문에 unwrap을 사용할 필요 없음
     if let Err(error) = run(config) {
+        eprintln!("Application error: {error}");
         process::exit(1);
     }
 }
